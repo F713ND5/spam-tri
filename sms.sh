be50ee1163de5cd4db7e5f29483285359b943b15
Fake Smile <64054240+Ivan-ID@users.noreply.github.com>
2020-04-21 19:41:53 +0700

    Create sms.sh

---
+++ sms.sh
@@ -0,0 +1,38 @@
+#latihan buat script sendiri om...
+#jangan recode mulu
+a="\033[30;1m"
+m="\033[31;1m"
+h="\033[32;1m"
+k="\033[33;1m"
+b="\033[34;1m"
+c="\033[35;1m"
+bm="\033[36;1m"
+p="\033[37;1m"
+m1="\033[38;1m"
+p7="\033[39;1m"
+hi="\033[40;1m"
+
+
+
+cd jangan-buka
+python2 sub.py
+
+clear
+echo "
+$c          /\
+$c         /  \       $m      *$a ctrl + z Untuk Stop$m *
+$c        /    \      $b ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+$c        [=][=]      $m   ╔══╗╔═╗╔══╗╔═╦═╗╔══╗╔═╦═╗╔══╗
+$c       |      |     $m   ║══╣║╬║║╔╗║║║║║║║══╣║║║║║║══╣
+$c       |      |     $p   ╠══║║╔╝║╠╣║║║║║║╠══║║║║║║╠══║
+$c      /|      |\    $p   ╚══╝╚╝─╚╝╚╝╚╩═╩╝╚══╝╚╩═╩╝╚══╝
+$c     / |      | \   $h     Coded By Ivan Firmansyah
+$c    /  |      |  \  $h         Azzura Chanel
+$c   /__ |      |___\ $b ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+$c       /       \    $h   ($a contoh nomor : 6289xxxxxxx $h)
+$c      /_&_____&_\   $m  JANGAN BANYAK BANYAK KALO NYEPAM
+$c       (=)   (=)
+
+"
+cd jangan-buka
+php 3.php
